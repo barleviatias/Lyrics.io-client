a4689d00bc21cc9430c89b32bb2e2e2847669194
barleviatias <barlevi.atias@gmail.com>
2023-07-11 14:56:40 +0300

    fix the list

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,21 +1,45 @@
+let api = 'https://localhost:7245/api/Songs';
+ajaxCall(
+	'GET',
+	api,
+	null,
+	(data) => {
+		renderSongs(data);
+	},
+	(err) => {
+		console.log(err);
+	}
+);
+
+function renderSongs(data) {
+	strHTML = ``;
+	const container = document.querySelector('.spotify-playlists'); // Replace 'container' with the ID of your container element
+
+	// Clear the container
+	container.innerHTML = '';
+	for (d of data) {
+		const item = document.createElement('div');
+		item.classList.add('item');
+
+		// Create and append the artist element
+		const artist = document.createElement('p');
+		artist.innerText = d.artist.trim();
+		item.appendChild(artist);
 
 
-let api = "https://localhost:7245/api/Songs";
-  ajaxCall(
-    "GET",
-    api,null,
-    (data) => {
+		// Create and append the song element
+		const song = document.createElement('p');
+		song.innerText = d.song.trim();
+		item.appendChild(song);
+    
+    const addToFavoritesButton = document.createElement('button');
+    addToFavoritesButton.innerHTML = '<i class="far fa-star"></i>';
+    addToFavoritesButton.addEventListener('click', () => {
+      addToFavorites(obj);
+    });
+    item.appendChild(addToFavoritesButton);
 
-      renderSongs(data);
-    },
-    (err) => {
-      console.log(err);
-    }
-    );
-
-    function renderSongs(data){
-        strHTML=``;
-        for (d of data){
-            console.log(d);
-        }
-    }
+		// Append the item to the container
+		container.appendChild(item);
+	}
+}
