df8e147434f59566b07dd2e4db49e19be21b4511
RoiValdiS <130987137+RoiValdiS@users.noreply.github.com>
2023-07-18 15:18:45 +0300

    kkk

---
+++ Scripts/adminPage.js
@@ -0,0 +1,36 @@
+let api = 'https://localhost:7245/';
+let users = []
+
+function getUsers(){
+    let getUsersAPI = api + 'api/Users';
+    ajaxCall("GET" , getUsersAPI , null , 
+    (data)=>{
+        console.log(data);
+        users= data;
+    },(err)=>{
+        alert(err);
+    });
+}
+
+
+function getFavoriteByID(){
+    let api3 = api+'api/Songs/GetFavByID?userId=' + 1;
+
+    ajaxCall('GET', api3, null,
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        arr(err);
+      }
+    );
+}
+function getFavorite(){
+    let favAPI= api+'api/Songs/GetAllFav';
+    ajaxCall("GET",countFavorite,null,
+    (data)=>{
+        console.log(data);
+    },(err)=>{
+        alert(err);
+    });
+}
